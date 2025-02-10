0cd55f8541f2e56cd26abe8dda9688c4a3b39684
Ryan Xu <ruoyu.xu.cn@windriver.com>
2025-02-10 17:39:41 +0800

    Add EVENT_MODULE_MAP,EVENT_CPU_ID,EVENT_WIND_EXIT_DISPATCH

--- src/trace_processor/importers/wvr/wvr_parser.cc
+++ src/trace_processor/importers/wvr/wvr_parser.cc
@@ -29,6 +29,8 @@
 #include "perfetto/ext/base/string_utils.h"
 #include "perfetto/ext/base/string_view.h"
 #include "perfetto/trace_processor/trace_blob_view.h"
+#include "src/trace_processor/importers/common/cpu_tracker.h"
+#include "src/trace_processor/importers/common/sched_event_tracker.h"
 #include "src/trace_processor/importers/common/process_tracker.h"
 #include "src/trace_processor/importers/common/slice_tracker.h"
 #include "src/trace_processor/importers/common/track_tracker.h"
@@ -59,12 +61,77 @@ base::Status WvrParser::Parse(TraceBlobView blob) {
 
   WVRFileReader reader(0, blob.size(), isLittleEndian);
 
+  std::map<uint64_t, Event> module_map;
+
+  vector<Event> eventCache;
+
+  bool isFirstTick = true;
+  int16_t currentCpuId = 0;
+  int64_t currentTicks = 0;
+  int64_t firstTicks = 0;
+  // int64_t prevTicks = 0;
+
   while (reader.parseEvent(src)) {
     Event event = reader.getCurrentEvent();
-    // 处理taskName
-    if (event.getId() == 3) {
-      int64_t taskId = 0;
-      int64_t rtpId = 0;
+    int64_t ticks = event.getTicks();
+    if (ticks != 0 && isFirstTick) {
+      firstTicks = ticks;
+      isFirstTick = false;
+    }
+    if (ticks != 0) {
+      currentTicks = currentTicks + ticks;
+    }
+
+    if (event.getId() == 20) {  // EVENT_MODULE_MAP
+      uint64_t rtpId = 0;
+      string name = "";
+      for (auto param : event.getParams()) {
+        vector<uint8_t> payload = param.getPayload();
+        if (param.getName() == "fileName") {
+          name = string(payload.begin(), payload.end());
+        } else if (param.getName() == "RtpId") {
+          uint8_t bytes[8];
+          memcpy(bytes, payload.data(), 8);
+          rtpId = reader.readUINT64(bytes);
+        }
+      }
+      std::cout << "Set RTP Name=" << name << " id=" << rtpId << std::endl;
+      ctx_->process_tracker->SetProcessNameIfUnset(
+          ctx_->process_tracker->GetOrCreateProcess(rtpId),
+          ctx_->storage->InternString(name));
+    } else if (event.getId() == 10) {  // EVENT_CPU_ID
+      for (auto param : event.getParams()) {
+        vector<uint8_t> payload = param.getPayload();
+        if (param.getName() == "cpuId") {
+          uint8_t bytes[2];
+          memcpy(bytes, payload.data(), 2);
+          currentCpuId = reader.readUINT16(bytes);
+        }
+      }
+    } else if (event.getId() == 52) {  // EVENT_WIND_EXIT_DISPATCH
+
+      uint64_t tid = 0;
+      uint64_t priority = 0;
+
+      for (auto param : event.getParams()) {
+        vector<uint8_t> payload = param.getPayload();
+        if (param.getName() == "tid") {
+          uint8_t bytes[8];
+          memcpy(bytes, payload.data(), 8);
+          tid = reader.readUINT64(bytes);
+        } else if (param.getName() == "priority") {
+          uint8_t bytes[8];
+          memcpy(bytes, payload.data(), 8);
+          priority = reader.readUINT64(bytes);
+        }
+      }
+
+      ctx_->sched_event_tracker->AddStartSlice(
+          currentCpuId, currentTicks, tid, priority);
+
+    } else if (event.getId() == 3) {  // EVENT_TASKNAME
+      uint64_t taskId = 0;
+      uint64_t rtpId = 0;
       int64_t cpuIndex = 0;
       string name = "";
       for (auto param : event.getParams()) {
@@ -73,22 +140,20 @@ base::Status WvrParser::Parse(TraceBlobView blob) {
           name = string(payload.begin(), payload.end());
         } else if (param.getName() == "taskId") {
           uint8_t bytes[8];
-          memcpy(bytes, payload.data(), sizeof(uint64_t));
+          memcpy(bytes, payload.data(), 8);
           taskId = reader.readUINT64(bytes);
         } else if (param.getName() == "rtpId") {
           uint8_t bytes[8];
-          memcpy(bytes, payload.data(), sizeof(uint64_t));
+          memcpy(bytes, payload.data(), 8);
           rtpId = reader.readUINT64(bytes);
         } else if (param.getName() == "cpuIndex") {
           uint8_t bytes[8];
-          memcpy(bytes, payload.data(), sizeof(uint64_t));
+          memcpy(bytes, payload.data(), 8);
           cpuIndex = reader.readUINT64(bytes);
         }
       }
 
-      ctx_->process_tracker->SetProcessNameIfUnset(
-          ctx_->process_tracker->GetOrCreateProcess(rtpId),
-          ctx_->storage->InternString("kernel"));
+      ctx_->process_tracker->GetOrCreateProcess(rtpId);
       auto utid = ctx_->process_tracker->UpdateThread(taskId, rtpId);
 
       StringId name_id = ctx_->storage->InternString(name);
